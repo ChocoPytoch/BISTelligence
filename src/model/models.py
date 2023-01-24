f1dc17c4a7547ce1629c7e658da993dd0d144687
dbnub <dbnubnub@gmail.com>
2023-01-24 13:18:26 +0000

    ryu commit_0124_2

--- src/model/models.py
+++ src/model/models.py
@@ -2,12 +2,14 @@
 
 import numpy as np
 import seaborn as sns
-from pyod.models.iforest import IForest
+import random
+import os
 from pyod.models.auto_encoder import AutoEncoder
 from pyod.models.mcd import MCD
+from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
 from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.mixture import GaussianMixture
 import tensorflow as tf
@@ -16,62 +18,190 @@ from tensorflow.keras import layers,losses,models
 from tensorflow.keras.models import Model, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
-def LR_Model(train=None, test=None):
-  model_lr = LinearRegression()
+class BaseModel:
 
-def MCD_Model(train=None,test=None):
-  model_mcd = "MCD"
-  model_mcd = MCD(support_fraction=0.8,contamination=0.01,random_state=42)
-  model_mcd.fit(train)
-  mcd_anomaly_score = model_mcd.decision_function(test)
-  sns.scatterplot(test.index,mcd_anomaly_score)
-  return model_mcd
+  def GetMCD(contamination,random_state):
 
-def OCSVM_Model(train=None,test=None):
-  model_ocsvm = "OCSVM"
-  model_ocsvm = OneClassSVM(nu=0.01)
-  model_ocsvm.fit(train)
-  ocsvm_anomaly_score = model_ocsvm.decision_function(test)
-  sns.scatterplot(test.index,ocsvm_anomaly_score)
-  return model_ocsvm
+    model_mcd = MCD(contamination=0.01,random_state=42)
 
-def LOF_Model(train=None,test=None):
-  model_lof = "LOF"
-  model_lof = LocalOutlierFactor(contamination=0.01,novelty=True)
-  model_lof.fit(train)
-  lof_anomaly_score = model_lof.decision_function(test)
-  sns.scatterplot(test.index,lof_anomaly_score)
-  return model_lof
+    return model_mcd
 
-def IForest_Model(train=None,test=None):
-  model_iforest = "IForest"
-  model_iforest = IForest(contamination=0.01,random_state=42,max_samples=300,n_estimators=200)
-  model_iforest.fit(train)
-  isof_anomaly_score = model_iforest.decision_function(test)
-  sns.scatterplot(test.index,isof_anomaly_score)
-  return model_iforest
+  def GetOCSVM(nu):
 
-def GM_Model(train=None,test=None):
-  model_gmm = "GMM"
-  lowest_bic = np.infty
-  bic = []
-  n_components_range = range(1,5)
-  cv_types = ["spherical", "tied", "diag", "full"]
-  for cv_type in cv_types:
-      for n_components in n_components_range:
-          gmm = GaussianMixture(
-              n_components=n_components, covariance_type=cv_type
-          )
-          gmm.fit(train)
-          bic.append(gmm.bic(train))
-          if bic[-1] < lowest_bic:
-              lowest_bic = bic[-1]
-              model_gmm = gmm
+    model_ocsvm = OneClassSVM(nu=0.01)
 
-  gmm_anomaly_score = model_gmm.score_samples(test)
-  sns.scatterplot(test.index,gmm_anomaly_score)
-  return model_gmm
+    return model_ocsvm
 
-def AE_Model(train=None,test=None):
-  model = "AE"
-  return model
+  def GetLOF(contamination, novelty):
+
+    model_lof = LocalOutlierFactor(contamination=0.01,novelty=True)
+
+    return model_lof
+
+  def GetIForest(contamination):
+    
+    model_iforest = IsolationForest(contamination = 0.01)
+
+    return model_iforest
+
+  def GetGMM(n_components, cv_type):
+    model_gmm = GaussianMixture(
+      n_components=n_components,
+      covariance_type=cv_type
+    )
+    return model_gmm
+
+  def GetAE(momentum, train_shape, random_state):
+    def my_seed_everywhere(seed: int = 2):
+      random.seed(seed) # random
+      np.random.seed(seed) # np
+      os.environ["PYTHONHASHSEED"] = str(seed) # os
+      tf.random.set_seed(seed) # tensorflow
+
+    my_seed = random_state
+    my_seed_everywhere(my_seed)
+
+    input_dim=train_shape[1]
+    #tf.random.set_seed(2)
+    initializer = tf.keras.initializers.HeNormal(seed=random_state)
+    momentum = momentum
+    
+    encoder=models.Sequential([
+        #input layer
+        layers.InputLayer(input_shape=input_dim),
+
+        layers.Dense(64,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(32,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(16,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(8,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(4,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+    ])
+
+    decoder=models.Sequential([ 
+        
+        layers.Dense(4,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(8,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(16,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(32,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        layers.Dense(64,kernel_initializer=initializer),
+        layers.BatchNormalization(momentum=momentum),
+        layers.ReLU(),
+        
+        #output layer
+        layers.Dense(input_dim,kernel_initializer=initializer),
+    ])
+
+    model_ae = models.Sequential([
+        encoder,decoder
+    ])
+
+    return model_ae
+
+class ModelTrain:
+
+  def __init__(self):
+    self.param_dict={'contamination':0.01,
+                     'nu':0.01,
+                     'novelty':True,
+                     'random_state':42,
+                     'n_conponents':1,
+                     'cv_type':'full',
+                     'momentum':0.9,
+                     'learning_rate':0.03,
+                     'epochs':100,
+                     'patience':10}
+
+  def SetTrainer(self, train_data):
+    self.train_data = train_data
+
+  def SetParam(self, param_dict={}):
+    self.param_dict.update(param_dict)
+
+  def GetTrainedModel(self,model_name="MCD"):
+    model = 0
+    if model_name == 'MCD':
+      model = BaseModel.GetMCD(
+        self.param_dict['contamination'],
+        self.param_dict['random_state']
+      )
+      model.fit(self.train_data)
+    
+    elif model_name == 'OCSVM':
+      model = BaseModel.GetOCSVM(
+        self.param_dict['nu']
+      )
+      model.fit(self.train_data)
+    
+    elif model_name == 'IForest':
+      model = BaseModel.GetIForest(
+        self.param_dict['contamination']
+      )
+      model.fit(self.train_data)
+
+    elif model_name == 'LOF':
+      model = BaseModel.GetLOF(
+        self.param_dict['contamination'],
+        self.param_dict['novelty']
+      )
+      model.fit(self.train_data)
+    
+    elif model_name == 'GMM':
+      model = BaseModel.GetGMM(
+        self.param_dict['n_components'],
+        self.param_dict['cv_type']
+      )
+      model.fit(self.train_data)
+    
+    elif model_name == 'AE':
+      model = BaseModel.GetAE(
+        self.param_dict['momentum'],
+        self.train_data.shape,
+        self.param_dict['random_state']
+      )
+      
+
+      early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1,patience=self.param_dict['patience'])
+      check_point = ModelCheckpoint('best_ae_model.h5',monitor='val_loss',mode='min',save_best_only=True)
+
+      model.compile(optimizer=keras.optimizers.Adam(learning_rate = self.param_dict['learning_rate']), 
+                        loss=keras.losses.MeanSquaredError())
+      history = model.fit(
+        self.train_data,self.train_data,
+        shuffle=True,
+        epochs=self.param_dict['epochs'],
+        batch_size=128,
+        validation_split = 0.3,
+        callbacks = [early_stopping, check_point],
+        verbose = 0
+    )
+
+    else:
+      raise 'you can choose in ["MCD", "IForest", "OCSVM", "LOF", "GMM", "AE"]'
+
+    return model
